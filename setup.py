cc6c2b865c095652a35fdd88d31f1c08efa14a54
Wis Chang <wistw.chang@gmail.com>
2020-09-07 00:08:07 +0800

    feat: fixed build pkg failed issue

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="technews-tw",
-    version="1.1.6",
+    version="1.1.7",
     description="Taiwan tech news crawlers.",
     author="Wis Chang",
     author_email="wistw.chang@gmail.com",
