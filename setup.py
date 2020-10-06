54f3ebe36a68dae00b39e6ad0f8baf0bddb5cd2b
Wis Chang <wistw.chang@gmail.com>
2020-10-06 21:22:09 +0800

    feat: update released version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="technews-tw",
-    version="1.2.2",
+    version="1.2.3",
     description="Taiwan tech news crawlers.",
     author="Wis Chang",
     author_email="wistw.chang@gmail.com",
